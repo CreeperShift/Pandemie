90de10e86ce97bd6e7b3b8f14181dc0bf207ece8
Max Henkes <henkes@stud.fra-uas.de>
2019-12-17 15:34:03 +0100

    Removed Refence from Actor

--- src/main/java/project/pandemie/logic/Actor.java
+++ src/main/java/project/pandemie/logic/Actor.java
@@ -3,7 +3,6 @@ package project.pandemie.logic;
 import project.pandemie.api.ILogic;
 import project.pandemie.data.Move;
 import project.pandemie.data.Round;
-import project.pandemie.logging.LogWriter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +18,6 @@ public class Actor implements ILogic {
 
         if(r.getRound() == 1){
             list.add(new Move.Builder("closeAirport").withCity("Abuja").withRounds(2).build());
-        }
-
-        LogWriter write =new LogWriter("D:/PandemieLog.txt", true);
-        try{
-            write.writeCity(r.getCities().get("Abuja"));
-        }catch(Exception e){
-            System.out.print(e);
         }
 
         //Debug Output
