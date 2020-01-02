81c8c4c74a0ad6257705214a13a48309deac83e9
CreeperShift <me@maxhenkes.com>
2020-01-02 06:56:13 +0100

    WIP Util to convert round to chart data. Might not be needed?

--- src/main/java/project/pandemie/util/UtilHelper.java
+++ src/main/java/project/pandemie/util/UtilHelper.java
@@ -1,5 +1,10 @@
 package project.pandemie.util;
 
+import org.knowm.xchart.XYChart;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class UtilHelper {
 
     private UtilHelper() {
@@ -35,6 +40,11 @@ public class UtilHelper {
         double a = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
         double c = 2 * Math.asin(Math.sqrt(a));
         return R * c;
+    }
+
+    public static ArrayList<XYChart> getChartData(HashMap<String, ArrayList<Integer>> data) {
+
+        return null;
     }
 
 }
