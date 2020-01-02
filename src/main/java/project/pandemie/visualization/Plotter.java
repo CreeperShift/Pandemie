8349238cfdcff761ea639dbd306fccb4c7945ff3
CreeperShift <me@maxhenkes.com>
2020-01-02 06:56:43 +0100

    Re-implemented Visualization. Now in proper java

---
+++ src/main/java/project/pandemie/visualization/Plotter.java
@@ -0,0 +1,76 @@
+package project.pandemie.visualization;
+
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.Styler;
+import project.pandemie.api.IVisual;
+import project.pandemie.data.Round;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Plotter implements IVisual {
+
+    static XYChart chartInstance;
+    static JPanel chartPanel;
+    private String title;
+    private String x;
+    private String y;
+    private int w;
+    private int h;
+
+    private boolean started = false;
+
+    private ArrayList<Integer> rounds = new ArrayList<>();
+    private ArrayList<Integer> population = new ArrayList<>();
+
+
+    public Plotter(String title, String x, String y, int w, int h) {
+        Visualizer.getVisualizerList().add(this);
+        this.title = title;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    @Override
+    public void visualize(Round r) {
+        population.add(r.getWorldPopulation());
+        if (started) {
+            chartInstance.updateXYSeries("world", null, population, null);
+            chartPanel.revalidate();
+            chartPanel.repaint();
+        } else {
+            chartInstance = new XYChartBuilder().width(w).height(h).title(title).xAxisTitle(x).yAxisTitle(y).build();
+            chartInstance.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line).setLegendPosition(Styler.LegendPosition.InsideSW).setInfoPanelVisible(true).setLegendVisible(true);
+            chartInstance.addSeries("world", null, population, null);
+
+            // Schedule a job for the event-dispatching thread:
+            // creating and showing this application's GUI.
+            javax.swing.SwingUtilities.invokeLater(() -> {
+
+                // Create and set up the window.
+                JFrame frame = new JFrame("Advanced Example");
+                frame.setLayout(new BorderLayout());
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                // chart
+                chartPanel = new XChartPanel<XYChart>(chartInstance);
+                frame.add(chartPanel, BorderLayout.CENTER);
+
+                // label
+                JLabel label = new JLabel("Blah blah blah.", SwingConstants.CENTER);
+                frame.add(label, BorderLayout.SOUTH);
+
+                // Display the window.
+                frame.pack();
+                frame.setVisible(true);
+            });
+            started = true;
+        }
+    }
+}
---
+++ src/main/java/project/pandemie/visualization/Visualizer.java
@@ -0,0 +1,60 @@
+package project.pandemie.visualization;
+
+import project.pandemie.api.IVisual;
+import project.pandemie.data.Round;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Visualizer {
+
+    private boolean newRound = true;
+
+    private static ArrayList<Collection<Round>> roundCollection = new ArrayList<>();
+
+    private static List<IVisual> visualizationList = new ArrayList<>();
+
+    private Collection<Round> rounds;
+
+    public static List<IVisual> getVisualizerList() {
+        return visualizationList;
+    }
+
+    public void addRound(Round r) {
+
+        if (newRound) {
+            rounds = new ArrayList<>();
+            rounds.add(r);
+            roundCollection.add(rounds);
+            newRound = false;
+            return;
+        }
+
+        if (!r.getOutcome().equalsIgnoreCase("pending")) {
+            rounds.add(r);
+            newRound = true;
+        } else {
+            rounds.add(r);
+        }
+        processVisualization(r);
+    }
+
+    private void processVisualization(Round r) {
+        visualizationList.forEach(o -> o.visualize(r));
+    }
+
+    public static Collection<Collection<Round>> getRoundCollection() {
+        return roundCollection;
+    }
+
+    public static Collection<Round> getRound(int round) {
+        if (round <= roundCollection.size()) {
+            return roundCollection.get(round - 1);
+        } else {
+            return null;
+        }
+    }
+
+
+}
